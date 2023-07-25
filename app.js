dc121fd1e295f41cc582815cc897e8e9ff081fb3
Charitha Saranga <108354430+CharithaSaranga@users.noreply.github.com>
2023-07-25 18:27:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ $(".autoplay").slick({
     },
   ],
 });
+$(document).ready(function () {
+  $("#form-btn").on("click", function () {
+    const email = $("#email-input").val().trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email === "") {
+      $(".error-message").text("Please enter an email address.");
+      $(".error-message").show();
+    } else if (!emailRegex.test(email)) {
+      $(".error-message").text("Please enter a valid email address.");
+      $(".error-message").show();
+    } else {
+      // Here, you can add code to submit the form or perform any other action.
+      // For example, you might want to use AJAX to send the form data to the server.
+      // If the form submission is successful, you can display a success message.
+      $(".error-message").hide();
+      alert("Form submitted successfully!");
+    }
+  });
+});
